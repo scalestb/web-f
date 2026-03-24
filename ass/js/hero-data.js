05d5a0df8a0a04577f3818a3ea481259dd5f35dd
BinhVPT 2026 Ub <97277476+scalestb@users.noreply.github.com>
2026-03-24 10:22:09 +0700

    upload news week

--- ass/js/hero-data.js
+++ ass/js/hero-data.js
@@ -1,36 +1,17 @@
 window.heroData = {
-    chip: "Tin nóng hỏi",
-    title: "MỞ THƯ YÊU - NHẬN QUÀ REAL",
-    description: "Tính đến 10/03, đã có gần 100.000 “bức thư yêu” từ Hi FPT được Quý Khách hàng khai mở, hàng chục nghìn món quà đã 'hữu duyên' với chủ nhân may mắn!",
-    actions: [
-        {
-            label: "Xem trên facebook",
-            href: "https://www.facebook.com/photo?fbid=972973768576359&set=a.277040668169676"
-        },
-        {
-            label: "Tải app ngay",
-            href: "https://hi.fpt.vn/rev/lbq/736eyDgY"
-        }
-    ],
-    stats: [
-        {
-            value: "95.242",
-            label: "Lượt mở thư"
-        },
-        {
-            value: "56.748",
-            label: "Khách hàng trải nghiệm tính năng"
-        },
-        {
-            value: "42.000",
-            label: "Món quà đã được trao"
-        }
-    ],
+    chip: "Hot News",
+    title: "MUA 6 TẶNG 2",
+    titleHref: "https://hi.fpt.vn/rev/flg/W6y6Y9vK",
+    description: "Tặng thêm 2 tháng khi mua 6 tháng gói FPT Play VVIP2 trên Hi FPT.",
+    featured: {
+        image: "ass/img/hot-news.png",
+        imageAlt: "Tặng thêm 2 tháng khi mua 6 tháng gói FPT Play VVIP2 trên Hi FPT."
+    },
     banner: {
         image: "ass/img/banner_9_4_2.png",
         imageAlt: "Banner HiFPT",
         title: "Hi FPT đã cập nhật version 9.4.2",
-        description: "Ứng dụng Hi FPT được ví như điều khiển thông minh, người bạn đồng hành tin cậy giúp khách hàng FPT quản lý toàn trình Dịch vụ Internet, Truyền hình, Camera và các dịch vụ khác mọi lúc, mọi nơi theo nhu cầu."
+        description: "Ứng dụng Hi FPT tiếp tục được nâng cấp để quản lý Internet, Truyền hình, Camera và các dịch vụ khác mọi lúc, mọi nơi."
     },
     note: {
         title: "Hotline hỗ trợ khách hàng",
