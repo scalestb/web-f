79c6584c216975bdb763ebe0cdbec5dc82007412
BinhVPT 2026 Ub <97277476+scalestb@users.noreply.github.com>
2026-04-21 15:20:36 +0700

    upload new page

--- ass/js/article-data.js
+++ ass/js/article-data.js
@@ -1,39 +1,32 @@
 window.articleItems = [
     {
-        id: "binh-luan",
+        image: "ass/img/post/post-2.jpg",
+        imageAlt: "Sao May",
+        title: "Sao May: thông điệp của bạn ngày hôm nay",
+        linkHref: "https://www.facebook.com/photo?fbid=1002492418957827&set=a.277040668169676"
+    },
+    {
+        image: "ass/img/post/post-1.png",
+        imageAlt: "Bóng hồng nhà F",
+        title: "Bóng hồng nhà F: Từ ‘chơi thử’ tới ‘out trình’",
+        linkHref: "https://chungta.vn/the-thao/bong-hong-nha-f-tu-choi-thu-toi-out-trinh-1141264.html"
+    },
+    {
         image: "ass/img/thanhdu.png",
-        imageAlt: "Thanh du",
-        category: "Fun is Life",
-        time: "13:40",
-        topic: "Gameshow",
+        imageAlt: "Thánh dự FTEL",
         title: "Thánh dự FTEL : Vui cùng ngoại hạng",
-        description: "Chào mừng bạn đến với FUN cùng Ngoại Hạng - Sân chơi dự đoán Ngoại hạng Anh dành riêng cho thành viên nhà Cáo",
-        linkHref: "https://ngoaihangnhacao.fpt.vn/",
-        linkLabel: "Tham gia ngay",
-        countLabel: "11673 đã tham gia"
+        linkHref: "https://ngoaihangnhacao.fpt.vn/"
     },
     {
         image: "ass/img/vuonhoa83.png",
         imageAlt: "Vườn hoa 8-3",
-        category: "Tin FPT Telecom",
-        time: "08:30",
-        topic: "8-3",
         title: "‘Vườn hoa xinh đẹp’ Viễn thông FPT rực rỡ khoe sắc trước thềm 8/3",
-        description: "Viễn thông FPT tổ chức cuộc thi ảnh “Em và Hoa”, là sân chơi nhằm tôn vinh phái đẹp, lan tỏa tinh thần tích cực, tự tin và tỏa sáng của chị em qua từng khoảnh khắc đời thường.",
-        linkHref: "https://www.ftel.fun/2026/03/vuon-hoa-xink-dep-vien-thong-fpt-ruc-ro-khoe-sac-truoc-them-8-3.html",
-        linkLabel: "Xem bài viết",
-        countLabel: "24 phản hồi"
+        linkHref: "https://www.ftel.fun/2026/03/vuon-hoa-xink-dep-vien-thong-fpt-ruc-ro-khoe-sac-truoc-them-8-3.html"
     },
     {
         image: "ass/img/bocongan.jpg",
-        imageAlt: "Ki hop tac",
-        category: "Tin FPT Telecom",
-        time: "10:15",
-        topic: "Nâng cao trải nghiệm",
+        imageAlt: "Ký hợp tác cùng Bộ Công an",
         title: "Viễn thông FPT ký hợp tác cùng Bộ Công an, tích hợp VNeID trên hệ sinh thái số nhà F",
-        description: "Viễn thông FPT và Trung tâm Nghiên cứu, Ứng dụng Dữ liệu Dân cư và Căn cước công dân (Trung tâm RAR) - Bộ Công an vừa tổ chức lễ ký kết Hợp đồng triển khai dịch vụ xác thực điện tử qua VNeID trên các ứng dụng Hi FPT, FangTV, FPT Life.",
-        linkHref: "https://chungta.vn/cong-nghe/vien-thong-fpt-ky-hop-tac-cung-bo-cong-an-tich-hop-vneid-tren-he-sinh-thai-so-nha-f-1140744.html",
-        linkLabel: "Xem bài viết",
-        countLabel: "180 lượt thả tim"
+        linkHref: "https://chungta.vn/cong-nghe/vien-thong-fpt-ky-hop-tac-cung-bo-cong-an-tich-hop-vneid-tren-he-sinh-thai-so-nha-f-1140744.html"
     }
 ];
--- ass/js/hero-data.js
+++ ass/js/hero-data.js
@@ -1,20 +1,22 @@
-window.heroData = {
-    chip: "Hot News",
-    title: "MUA 6 TẶNG 2",
-    titleHref: "https://hi.fpt.vn/rev/flg/W6y6Y9vK",
-    description: "Tặng thêm 2 tháng khi mua 6 tháng gói FPT Play VVIP2 trên Hi FPT.",
-    featured: {
-        image: "ass/img/hot-news.png",
-        imageAlt: "Tặng thêm 2 tháng khi mua 6 tháng gói FPT Play VVIP2 trên Hi FPT."
+window.heroData = [
+    {
+        chip: "Hot News",
+        title: "Thánh dự FTEL",
+        titleHref: "https://ngoaihangnhacao.fpt.vn/",
+        description: "Trổ tài dự đoán cùng Nhà Cáo",
+        featured: {
+            image: "ass/img/banner/ngoaihang.png",
+            imageAlt: "Trổ tài dự đoán cùng Nhà Cáo"
+        }
     },
-    banner: {
-        image: "ass/img/banner_9_4_2.png",
-        imageAlt: "Banner HiFPT",
-        title: "Hi FPT đã cập nhật version 9.4.2",
-        description: "Ứng dụng Hi FPT tiếp tục được nâng cấp để quản lý Internet, Truyền hình, Camera và các dịch vụ khác mọi lúc, mọi nơi."
-    },
-    note: {
-        title: "Hotline hỗ trợ khách hàng",
-        description: "1900 6600"
+    {
+        chip: "Games",
+        title: "Thông điệp vũ trụ dành cho bạn",
+        titleHref: "https://hi.fpt.vn/rev/lbq/736eyDgY",
+        description: "Một thông điệp dành riêng cho bạn đang chờ được đón nhận.",
+        featured: {
+            image: "ass/img/banner/sao-may.png",
+            imageAlt: "Sao May"
+        }
     }
-};
+];
