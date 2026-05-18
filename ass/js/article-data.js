72743c990698ea1ee344cbcd187e557274da7169
BinhVPT 2026 Ub <97277476+scalestb@users.noreply.github.com>
2026-05-18 17:31:05 +0700

    upload

--- ass/js/article-data.js
+++ ass/js/article-data.js
@@ -28,5 +28,77 @@ window.articleItems = [
         imageAlt: "Ký hợp tác cùng Bộ Công an",
         title: "Viễn thông FPT ký hợp tác cùng Bộ Công an, tích hợp VNeID trên hệ sinh thái số nhà F",
         linkHref: "https://chungta.vn/cong-nghe/vien-thong-fpt-ky-hop-tac-cung-bo-cong-an-tich-hop-vneid-tren-he-sinh-thai-so-nha-f-1140744.html"
+    },
+    {
+        image: "https://i.chungta.vn/2026/04/07/-4619-1775537550.jpg",
+        imageAlt: "TechHub",
+        title: "TechHub: Nền tảng ‘trao quyền’ và khát vọng kiến tạo cộng đồng công nghệ duy nhất FPT",
+        linkHref: "https://chungta.vn/cong-nghe/techhub-nen-tang-trao-quyen-va-khat-vong-kien-tao-cong-dong-cong-nghe-duy-nhat-fpt-1141221.html"
+    },
+    {
+        image: "https://i.chungta.vn/2026/02/04/-7917-1770201056.jpg",
+        imageAlt: "Top 100 FPT 2025",
+        title: "Anh Nguyễn Thế Quang là Hoa hậu FPT 2025",
+        linkHref: "https://chungta.vn/nguoi-fpt/anh-nguyen-the-quang-la-hoa-hau-fpt-2025-1140910.html"
+    },
+    {
+        image: "https://i.chungta.vn/2026/01/28/-9636-1769593349.jpg",
+        imageAlt: "FPT Telecom 2026",
+        title: "Anh Hoàng Việt Anh: 2026 là năm Viễn thông FPT vươn mình thành doanh nghiệp lưỡng dụng",
+        linkHref: "https://chungta.vn/nguoi-fpt/anh-hoang-viet-anh-2026-la-nam-vien-thong-fpt-vuon-minh-thanh-doanh-nghiep-luong-dung-1140864.html"
+    },
+    {
+        image: "https://i.chungta.vn/2025/12/19/-2438-1766133406.jpg",
+        imageAlt: "FPT Wi-Fi 7",
+        title: "FPT là Nhà cung cấp giải pháp Internet Wi-Fi 7 được Tinh Tế bình chọn",
+        linkHref: "https://chungta.vn/cong-nghe/fpt-la-nha-cung-cap-giai-phap-internet-wi-fi-7-duoc-tinh-te-binh-chon-1140693.html"
+    },
+    {
+        image: "https://i.chungta.vn/2025/04/14/-8854-1744613769.jpg",
+        imageAlt: "Viễn thông FPT tăng trưởng",
+        title: "Tăng trưởng vượt bậc, Viễn thông FPT nhận bằng khen từ Tập đoàn",
+        linkHref: "https://chungta.vn/nguoi-fpt/tang-truong-vuot-bac-vien-thong-fpt-nhan-bang-khen-tu-tap-doan-1139710.html"
+    },
+    {
+        image: "https://i.chungta.vn/2024/07/22/-2908-1721582164.jpg",
+        imageAlt: "FPT Telecom Futsal",
+        title: "FPT Telecom vô địch Futsal FPT sau 7 năm",
+        linkHref: "https://chungta.vn/the-thao/fpt-telecom-vo-dich-futsal-fpt-sau-7-nam-1138896.html"
+    },
+    {
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgZVqfMaeCJRSJO9L-07Xv2EZUDRlI2Qg18MHTddCgs7uGG8PudMABmGeVK8K3nmjkNcIMUUtqsNhVbs2dxK8ZycGQNG8-_Or2ktvf1td3VrO75U40zH1rqwobEB7j_FK2iEJ86meQehVaP9uumu7A3_NYi_jVFP6GSum_zzYy8aw8lzlXeYLoDKwVJv1g/s16000/615302846_1412632923552944_551178453165148142_n.jpg",
+        imageAlt: "Slogan Logo FTEL30",
+        title: "FPT Telecom phát động cuộc thi sáng tác Slogan & Logo FTEL30",
+        linkHref: "https://www.ftel.fun/2026/01/fpt-telecom-phat-dong-cuoc-thi-sang-tac-slogan-va-logo-ftel30.html"
+    },
+    {
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj2_92Z9J6qi31NQEnF-baPYVgoE9l7xfu4hjc7UjCaT5EvGPFi-hUfIqVPuf7LlNzguTWTquxRgfB0lr94em8lkCfvNpucQE3eUve1OQnHRPaTdcV8utu2DtBHjQ1vSu55LUTNiTiPwIaAAbFaFd3-r4Vg9ogtCgECOg-XpwsgT9dVKVarhufviT46MWM/s16000/Untitled%20design.jpg",
+        imageAlt: "Hương Sắc Nhà Cáo",
+        title: "Rực rỡ sắc màu tại cuộc thi Cắm Hoa “Hương Sắc Nhà Cáo” tại Hà Nội và Hồ Chí Minh",
+        linkHref: "https://www.ftel.fun/2026/03/ruc-ro-sac-mau-tai-cuoc-thi-cam-hoa-Nha-Cao.html"
+    },
+    {
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEinctXmtvBRwY12k3-J4bK5cjSttyUMiENxW_JJfNHUajl1kZjaVaEWyj9vclNc-83VBlq6S9g-6G6u87kGu4s7RpYBtFtCvwLv-ZPG3lw4A_OisKhMuob6dJ3fVMIQh99Kg6k2G_hHDSrSfwlIHCB86Bc2KvEDeahY2DGV-R3F7stgFuC8qHY3VgLOiRw/s16000/1.png",
+        imageAlt: "Ngày ấy Bây giờ",
+        title: "Khởi động Phong trào “Ngày ấy Bây giờ”: Cùng nhau Xuôi miền ký ức",
+        linkHref: "https://www.ftel.fun/2026/03/khoi-dong-phong-trao-ngay-ay-bay-gio-cung-nhau-xuoi-mien-ky-uc.html"
+    },
+    {
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiMc_O3-WXPLE8rzyAt55OyCAMVwx_6J1Hisz810MAmGdApoNyzaDtBvAduLciOkCnN7rlwHxCx7x2o6kDXczL9Zt1c6nt1b7X6wkqkLqUoGgvVVuDeTWMs_GWxUZ7oCMLfLxBoHmXMu6MOM-BGJa64Uh2BA0FQNYEtJCEj2kmoqtGovxLncu6Cv-pgQjw/s16000/70591430f44c75122c5d.jpg",
+        imageAlt: "Quán quân Ngày ấy Bây giờ",
+        title: "“Đường có thể xa, nhưng đi rồi sẽ đến” - Lời giải cho bài toán thanh xuân của Quán quân “Ngày ấy - Bây giờ”",
+        linkHref: "https://www.ftel.fun/2026/04/duong-co-the-xa-nhung-cu-di-roi-se-den-loi-giai-cho-bai-toan-thanh-xuan-cua-quan-quan-ngay-ay-bay-gio.html"
+    },
+    {
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhJ5zrtbT7BLDHfo9q810Zgi59hf3GdLnHUVR0VaYlSUpOKxK7zsvkGgFMH5z2M3hsIP5xB7FarSu6Sz7SP4NJ38V7BMCWVozwEInstEMxRbCMCDJz1ZlmGoBokMF38BMfzJ0LWI1AA_QCoc6qot4nL2jmB306fVPrxl2C4sxdEOIKc2TYh7AeTgRlGw8M/s16000/94d67e05ff767e282767.jpg",
+        imageAlt: "Tổng kết Ngày ấy Bây giờ",
+        title: "Tổng kết giải thưởng cuộc thi “Ngày Ấy - Bây Giờ”: Ghi dấu hành trình 30 năm một sứ mệnh kết nối",
+        linkHref: "https://www.ftel.fun/2026/04/tong-ket-va-cong-bo-giai-thuong-cuoc-thi-ngay-ay-bay-gio-ghi-dau-hanh-ytinh-30-nam-mot-su-menh-ket-noi.html"
+    },
+    {
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgTg8xN0zB4kijs-dF04ijGdJ7nTdSvOgA2-45UbjtrTZrY0HwrNK1mlofbuZ-fFpcugT3ZnvZt9nyejCQkOKBSasJad2ATDPI78QIMIV8hZYWnCXhiszyZFB9WUX3aaIIGNKtxznwJVe_C_a90Yp1OLUbQj7KAM9dyykYdg_sytFGc4q66q14TvV9RnRo/s16000/IMG_4356.jpg",
+        imageAlt: "Đồng Đội FPT Telecom",
+        title: "Đồng Đội FPT Telecom cùng Quỹ Hy Vọng thắp sáng ước mơ tại mái trường Vùng Cao A Lưới",
+        linkHref: "https://www.ftel.fun/2026/04/Dong-doi-FTEL-Cung-quy-Hy-Vong-thap-sang-uoc-mo-tai-mai-truong-vung-cao-a-luoi.html"
     }
 ];
